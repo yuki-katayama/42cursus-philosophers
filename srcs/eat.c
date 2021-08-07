1158774e80a4dcef5352fa4b5f03ab030414ec8f
yuki-katayama <katayu810@gmail.com>
2021-08-07 19:50:19 +0900

    refactor: ok norm in eat.c

--- srcs/eat.c
+++ srcs/eat.c
@@ -1,23 +1,26 @@
 #include "philosopher.h"
 
-int ft_eat(t_philo *philo)
+int	ft_eat(t_philo *philo)
 {
+	pthread_mutex_t	*right;
+	pthread_mutex_t	*left;
+
+	right = philo->info->philo[(philo->id + 1) % \
+								philo->info->num_philo].eating_m;
+	left = philo->info->philo[(philo->id - 1) % \
+								philo->info->num_philo].eating_m;
 	if (philo->info->status.died == FALSE)
 	{
-		pthread_mutex_lock(philo->info->philo[(philo->id + 1) %
-											  philo->info->num_philo]
-							   .eating_m);
-		pthread_mutex_lock(philo->info->philo[(philo->id - 1) %
-											  philo->info->num_philo]
-							   .eating_m);
+		pthread_mutex_lock(right);
+		pthread_mutex_lock(left);
 		philo->eating = 1;
 		philo->time_last_eat = ft_gettime(philo);
 		if (ft_output(philo, EAT) == ERROR)
 			return (ERROR);
 		if (ft_action_usleep(philo->info->times.time_eat, philo) == ERROR)
 			return (ERROR);
-		pthread_mutex_unlock(philo->info->philo[(philo->id - 1) % philo->info->num_philo].eating_m);
-		pthread_mutex_unlock(philo->info->philo[(philo->id + 1) % philo->info->num_philo].eating_m);
+		pthread_mutex_unlock(left);
+		pthread_mutex_unlock(right);
 		philo->limit_eats -= 1;
 		if (philo->limit_eats == 0)
 			philo->info->status.philos_limit_eats += 1;
