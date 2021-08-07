e8fd65d17d081dd9837f56abe8945d2b74b1cfdb
yuki-katayama <katayu810@gmail.com>
2021-08-07 14:23:46 +0900

    add: Only do eat-process when not died

--- srcs/eat.c
+++ srcs/eat.c
@@ -1,24 +1,25 @@
 #include "philosopher.h"
 
-int	ft_eat(t_philo *philo)
+int ft_eat(t_philo *philo)
 {
-	pthread_mutex_lock(philo->info->philo[(philo->id + 1) \
-						% philo->info->num_philo].eating_m);
-	pthread_mutex_lock(philo->info->philo[(philo->id - 1) \
-						% philo->info->num_philo].eating_m);
-	philo->eating = 1;
-	philo->time_last_eat = ft_gettime(philo);
-	if (ft_output(philo, EAT) == ERROR)
-		return (ERROR);
-	if (ft_action_usleep(philo->info->times.time_eat, philo) == ERROR)
-		return (ERROR);
-	pthread_mutex_unlock(philo->info->philo[(philo->id - 1) \
-						% philo->info->num_philo].eating_m);
-	pthread_mutex_unlock(philo->info->philo[(philo->id + 1) \
-						% philo->info->num_philo].eating_m);
-	philo->limit_eats -= 1;
-	if (philo->limit_eats == 0)
-		philo->info->status.philos_limit_eats += 1;
-	philo->eating = 0;
+	pthread_mutex_lock(philo->info->philo[(philo->id + 1) %
+						philo->info->num_philo].eating_m);
+	pthread_mutex_lock(philo->info->philo[(philo->id - 1) %
+						philo->info->num_philo].eating_m);
+	if (philo->info->status.died == FALSE)
+	{
+		philo->eating = 1;
+		philo->time_last_eat = ft_gettime(philo);
+		if (ft_output(philo, EAT) == ERROR)
+			return (ERROR);
+		if (ft_action_usleep(philo->info->times.time_eat, philo) == ERROR)
+			return (ERROR);
+		pthread_mutex_unlock(philo->info->philo[(philo->id - 1) % philo->info->num_philo].eating_m);
+		pthread_mutex_unlock(philo->info->philo[(philo->id + 1) % philo->info->num_philo].eating_m);
+		philo->limit_eats -= 1;
+		if (philo->limit_eats == 0)
+			philo->info->status.philos_limit_eats += 1;
+		philo->eating = 0;
+	}
 	return (0);
 }
