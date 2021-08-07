500dce0ca40cb0981edad21df4563fcea1e650ae
yuki-katayama <katayu810@gmail.com>
2021-08-07 15:05:45 +0900

    add: destroy eats_m and forks by mutex_destroy

--- srcs/exit.c
+++ srcs/exit.c
@@ -9,15 +9,11 @@ void ft_exit(t_info *info)
 	err = 0;
 	while (++i < info->num_philo)
 	{
-		if (pthread_mutex_unlock(&info->forks[i]) != 0)
+		if (pthread_mutex_destroy(&info->eats_m[i]) != 0
+			|| pthread_mutex_destroy(&info->forks[i]) != 0)
 		{
 			printf("%da\n", err);
-			ft_error(11);
-		}
-		if (pthread_mutex_destroy(&info->forks[i]) != 0)
-		{
-			printf("%da\n", err);
-			ft_error(11);
+			// ft_error(11);
 		}
 	}
 	if (pthread_mutex_destroy(&info->status.died_m) != 0 || pthread_mutex_destroy(&info->status.writing) != 0)
