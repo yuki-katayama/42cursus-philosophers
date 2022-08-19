24e537152002a710cb4b5914846761c741f0c8ac
yuki-katayama <katayu810@gmail.com>
2022-08-19 15:11:44 +0000

    fix: ワカモレはint64_tが%lldだった

--- philo/srcs_retry/print_status.c
+++ philo/srcs_retry/print_status.c
@@ -5,20 +5,20 @@ int8_t	ft_print_status(t_print *print_data)
 	if (print_data->philo->data->died != 1)
 	{
 		if (print_data->status == FORK)
-			printf("%s%ld %d has taken a fork%s\n", \
+			printf("%s%lld %d has taken a fork%s\n", \
 				GREEN, ft_gettime(), print_data->philo->id, DEFAULT);
 		else if (print_data->status == EAT)
-			printf("%s%ld %d is eating%s\n", \
+			printf("%s%lld %d is eating%s\n", \
 				YELLOW, ft_gettime(), print_data->philo->id, DEFAULT);
 		else if (print_data->status == SLEEP)
-			printf("%s%ld %d is sleeping%s\n", \
+			printf("%s%lld %d is sleeping%s\n", \
 				BLUE, ft_gettime(), print_data->philo->id, DEFAULT);
 		else if (print_data->status == THINK)
-			printf("%ld %d is thinking\n", \
+			printf("%lld %d is thinking\n", \
 				ft_gettime(), print_data->philo->id);
 		else if (print_data->status == DIED)
 		{
-			printf("%s%ld %d died%s\n", \
+			printf("%s%lld %d died%s\n", \
 				RED, ft_gettime(), print_data->philo->id, DEFAULT);
 		}
 	}
